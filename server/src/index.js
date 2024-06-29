491982e32bdc164f036ddc889866a74bdac4822a
Uri <uri555459@gmail.com>
2024-06-29 12:20:25 +0300

    Added function getUser and check token

--- server/src/index.js
+++ server/src/index.js
@@ -1,11 +1,26 @@
 import 'dotenv/config'
 import express from 'express'
 import { ApolloServer } from 'apollo-server-express'
+import jwt from 'jsonwebtoken'
+
 import { typeDefs } from './schema.js'
 
 import { connect } from './db.js'
 import { models } from './models/index.js'
 import { resolvers } from './resolvers/index.js'
+
+// Получаем информацию пользователя из JWT
+const getUser = token => {
+	if (token) {
+		try {
+			// Возвращаем информацию пользователя из токена
+			return jwt.verify(token, process.env.JWT_SECRET)
+		} catch (err) {
+			// Если с токеном возникла проблема, выбрасываем ошибку
+			new Error('Session invalid')
+		}
+	}
+}
 
 // Запускаем сервер на порте, указанном в файле .env, или на порте 4000
 const port = process.env.PORT || 4000
@@ -22,9 +37,15 @@ const start = async () => {
 	const server = new ApolloServer({
 		typeDefs,
 		resolvers,
-		context: () => {
-			// Добавление моделей БД в context
-			return { models }
+		context: ({ req }) => {
+			// Получаем токен пользователя из заголовков
+			const token = req.headers.authorization
+			// Пытаемся извлечь пользователя с помощью токена
+			const user = getUser(token)
+			// Пока что будем выводить информацию о пользователе в консоль:
+			console.log(user)
+			// Добавляем модели БД и пользователя в контекст
+			return { models, user }
 		},
 	})
 
